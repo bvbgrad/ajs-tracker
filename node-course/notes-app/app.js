1d146679a505b39eb9996f75a2c1ca2eaef29a4a
Brent v Bingham <bvbgrad@gmail.com>
2022-02-12 10:48:15 +0000

    Save status

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,7 +1,16 @@
 const validator = require('validator');
+const yargs = require('yargs');
+
 const getNotes = require('./notes.js');
 
 const msg = getNotes()
 console.log(msg)
+console.log(process.argv)
+console.log(yargs.argv)
 
-console.log(validator.isURL('https://msdead.io'))
+// const command = process.argv[2]
+// if (command === 'add') {
+//     console.log('Adding note')
+// } else if (command === 'remove') {
+//     console.log('Removing note')
+// }

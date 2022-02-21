491aca6da03a070ca49098c3c63629fb9c4549e7
Brent v Bingham <bvbgrad@gmail.com>
2022-02-21 09:33:47 +0000

    udemy-js#52 Section 7 basic Web server

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve static content
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -23,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'Help message'
+        message: 'Help message',
+        title: 'Help',
+        name: 'Brent Bingham'
     })
 })
 
@@ -31,6 +43,22 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
         location: 'Philedelphia'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        message: 'Help article not found',
+        title: '404',
+        name: 'Brent Bingham'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        message: 'Page not found',
+        title: '404',
+        name: 'Brent Bingham'
     })
 })
 

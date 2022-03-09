225e53d1343df9bc43ec888122dbe6a30bf4c224
Brent v Bingham <bvbgrad@gmail.com>
2022-03-09 08:23:55 +0000

    udemy.js#90 Create endpoints

---
+++ node-course/task-manager/src/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            // if (!validator.isStrongPassword(value)) {
+            //     throw new Error ('Password must have more than 8 characters, with at least 1 of each: lower case, upper case, number, and a symbol')
+            // } 
+            if (value.toLowerCase().includes('password')) {
+                throw new Error ('Password can not contain "password"')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+module.exports = User

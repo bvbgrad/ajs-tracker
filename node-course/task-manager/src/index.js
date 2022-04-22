c11bdff5b78ae8057df49b8cbda2280af8df12ba
Brent v Bingham <bvbgrad@gmail.com>
2022-04-22 04:08:23 +0000

    udemy.js#129 Section 14 Upload & CRUD user avatar

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -6,11 +6,6 @@ const userRouter = require('./routers/user')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     console.log(req.method, req.path)
-//     next()
-// })
-
 app.use(express.json())
 app.use(taskRouter)
 app.use(userRouter)
@@ -18,14 +13,3 @@ app.use(userRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-// const User = require('./models/user')
-
-// const main = async () => {
-//     const user = await User.findById('6253d9b562e7ab2ccbe44a11')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()

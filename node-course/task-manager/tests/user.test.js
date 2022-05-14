c8b004645fcfec23ba67aaac003b68a2ed5b741e
Brent V Bingham <bvbgrad@gmail.com>
2022-05-14 09:14:29 +0000

    udemy.js #144 Testing Authentication broken

--- node-course/task-manager/tests/user.test.js
+++ node-course/task-manager/tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'userOne',
     email: 'userOne@example.com',
-    password: 'userOne1234!'
+    password: 'userOne1234!',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -32,5 +39,16 @@ test('Should not login nonexistent user', async () => {
     await request(app).post('/users/login').send({
         email: userOne.email,
         password: "dummypassword"
-    }).expect(400)
+    }).expect(401)
+})
+
+test('Should get user profile', async () => {
+    await request(app).get('/users/me')
+        .set('Authorization', 'Bearer ${UserOne.tokens[0].token}')
+        .send().expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app).get('/users/me')
+        .send().expect(401)
 })
